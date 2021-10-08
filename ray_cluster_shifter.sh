5ff05750cb04c9469b731b012f1a1944bcb103c8
Matt Henderson <mhenderson@lbl.gov>
2021-10-07 20:13:50 -0700

    update for distributed ray tune with tensorflow

--- ray_cluster_shifter.sh
+++ ray_cluster_shifter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH -C gpu
-#SBATCH -c 80
-#SBATCH -G 8
-#SBATCH --nodes 2
+#SBATCH -N 2
 #SBATCH --ntasks-per-node=1
-#SBATCH --image=nersc/tensorflow:ngc-21.03-tf2-v0
+#SBATCH --cpus-per-task=80
+#SBATCH --gpus-per-task=8
+#SBATCH --image=nersc/tensorflow:ngc-21.05-tf2-v0
 #SBATCH --exclusive
 #SBATCH -t 02:00:00
 #SBTACH -J disk_det
 #SBATCH -o ray_test.out
 #SBATCH -e ray_error.err
 #SBATCH -A m2571
+#SBATCH -L SCRATCH
 
 #set up modules
 module load cgpu
@@ -19,8 +20,8 @@ module load cuda/shifter
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-train=$(readlink -f unrot_augmented_train.tfrecords)
-test=$(readlink -f unrot_augmented_test.tfrecords)
+train="unrot_augmented_train.tfrecords"
+test="unrot_augmented_test.tfrecords"
 
 #set up environment
 export PATH=/opt/shifter/bin:${PATH}
@@ -37,15 +38,38 @@ export redis_password
 nodes=$(scontrol show hostnames $SLURM_JOB_NODELIST) # Getting the node names
 nodes_array=( $nodes )
 
-node_1=${nodes_array[0]} 
-ip=$(srun --nodes=1 --ntasks=1 -w $node_1 hostname --ip-address) # making redis-address
+head_num_cpus=$(($SLURM_CPUS_PER_TASK - 4))
+
+node_1=${nodes_array[0]}
+ip=$(hostname -i | cut -d' ' -f1) # making redis-address
 port=6379
 ip_head=$ip:$port
-export ip_head
+export ip_head ip_port
 echo "IP Head: $ip_head"
 
-echo "STARTING HEAD at $node_1"
-srun --nodes=1 --ntasks=1 -w $node_1 shifter ray start --block --head --node-ip-address=$ip --port=6379 --redis-password=$redis_password &
+# Optional step to monitor the memory and activity of the head node while running
+#nvidia-smi --query-gpu=index,timestamp,name,pci.bus_id,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 5 -f "smi_profile-${SLURM_JOB_ID}.csv" &
+
+#export RAY_BACKEND_LOG_LEVEL=debug
+#export RAY_PROFILING=1
+# this enables the flag for GPU memory to be allocated as needed
+export TF_FORCE_GPU_ALLOW_GROWTH=true
+# this limits the number of tune results that will be queued to limit memory usage
+export TUNE_RESULT_BUFFER_LENGTH=10
+
+# This sets up the tensorflow config for the head node
+TF_NODES=$(jq -ncR '[inputs]' <<< "${nodes}")
+export TF_CONFIG="{\"cluster\": {\"worker\": $TF_NODES, \"task\": {\"type\": \"worker\", \"index\": 0}}"
+
+# Notes:
+# the ulimit is needed to avoid file limits from all of the task workers that can be created
+# num_cpus and num_gpus should be explicitly set to avoid overallocation problems
+echo "STARTING HEAD at $node_1 $ip:$port with cpus: ${head_num_cpus}, gpus: ${SLURM_GPUS_PER_TASK}"
+shifter --env PYTHONUSERBASE=${PYTHONUSERBASE} \
+     bash -c 'ulimit -n 65536; ray start -v --head --node-ip-address "$ip" --dashboard-host "$ip" --port "$port" \
+     --redis-password "$redis_password" --temp-dir "$SCRATCH/raytmp" \
+     --num-cpus "$head_num_cpus" --num-gpus "$head_num_gpus" --block' &
+echo "Head started, waiting before starting $worker_num workers..."
 sleep 30
 
 worker_num=$(($SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
@@ -53,13 +77,43 @@ echo "NODE OTHER THAN HEAD NODE $worker_num"
 for ((  i=1; i<=$worker_num; i++ ))
 do
   node_i=${nodes_array[$i]}
-  echo "STARTING WORKER $i at $node_i"
-  srun --nodes=1 --ntasks=1 -w $node_i shifter ray start --block --address=$ip_head --redis-password=$redis_password &
-  sleep 5
+  # set the tensorflow config for this worker, it will be passed in to shifter and then reset
+  export TF_CONFIG="{\"cluster\": {\"worker\": $TF_NODES,\"task\": {\"type\": \"worker\", \"index\": $i}}"
+
+  echo "STARTING WORKER $i at $node_i with $node_i_gpus gpus"
+  srun -C gpu --nodes=1 --ntasks=1 -c ${SLURM_CPUS_PER_TASK} --cpu-bind=cores --gpus-per-task=${SLURM_GPUS_PER_TASK} -w "$node_i" \
+       shifter --env PYTHONUSERBASE=${PYTHONUSERBASE} \
+       bash -c 'ulimit -n 65536; ray start -v --address "$ip_head" --redis-password "$redis_password" --temp-dir "$SCRATCH/raytmp" \
+       --num-cpus ${SLURM_CPUS_PER_TASK} --num-gpus "$node_i_gpus" --block' &  sleep 5
+  echo "WORKER $i STARTED"
+  # pause to give the workers time to sync with the head node
+  sleep 30
 done
 ##############################################################################################
 
 #### call your code below
+# reset the head node tensorflow config again for the client code
+export TF_CONFIG="{\"cluster\": {\"worker\": $TF_NODES, \"task\": {\"type\": \"worker\", \"index\": 0}}"
 
-shifter python -u tune_training.py --train-dataset=$train --test-dataset=$test
+start_time="$(date -u +%s)"
+echo "STARTING TASKS..."
+export RAY_ADDRESS=$ip_head
+RAY_NUM_CPUS_PER_WORKER=9
+RAY_NUM_GPUS_PER_WORKER=1
+export ray_num_workers=$((${SLURM_JOB_NUM_NODES} * ${SLURM_GPUS_PER_TASK}))
+# set the tensorflow concurrency limit for trials to the total number of workers
+export TUNE_MAX_PENDING_TRIALS_PG=${ray_num_workers}
+shifter --env PYTHONUSERBASE=${PYTHONUSERBASE} \
+        --env redis_password="$redis_password" \
+        bash -c 'python -u tune_training.py --train-dataset "$train" --test-dataset "$test" --num-ray-hosts ${#nodes[@]} --num-ray-workers-per-host ${SLURM_GPUS_PER_TASK} --num-ray-cpus-per-worker "$RAY_NUM_CPUS_PER_WORKER" --num-ray-gpus-per-worker "$RAY_NUM_GPUS_PER_WORKER"'
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "TASKS COMPLETED after $elapsed seconds"
+
+# clean up the head process
+shifter --env PYTHONUSERBASE=${PYTHONUSERBASE} \
+     bash -c 'ray stop'
+
+#pkill nvidia-smi
+
 exit
--- tune_training.py
+++ tune_training.py
@@ -1,79 +1,81 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import os
-import sys
-import time
-import uuid
-import logging
-import h5py
+# Python 2 compat?
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+
+# standard libraries
 import argparse
+import logging
+import os
+import time
+
+from typing import Dict
+
+# 3rd party
 import numpy as np
-from tqdm import tqdm
-
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]="true"
 import tensorflow as tf
-
 import tensorflow_addons as tfa
 
-print('Loading complex convolution, pooling libraries ... \n')
+try:
+    import GPUtil
+except ImportError:
+    GPUtil = None
 
+print('Loading ray Tune ... \n')
+import ray
+from ray import tune
+from ray.tune import CLIReporter, Callback
+from ray.tune.integration.keras import TuneReportCheckpointCallback
+from ray.tune.integration.tensorflow import (DistributedTrainableCreator, get_num_workers)
+from ray.tune.logger import LoggerCallback
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune.suggest import ConcurrencyLimiter
+from ray.tune.suggest.hyperopt import HyperOptSearch
+
+print('Loading complex convolution, pooling libraries ... \n')
 #Custom libraries - User defined
-import convSpectral
-import poolSpectral
-import augmentation
 from conv_utils import parseDataset
 from conv_utils import convSpec2d_block, conv2d_block
 from conv_utils import cross_correlate_iff, cross_correlate_ff
-from conv_utils import CustomSSIML1ELoss
-
-print('Loading ray Tune ... \n')
-from filelock import FileLock
-
-import ray
-from ray import tune
-from ray.tune.suggest.hyperopt import HyperOptSearch
-from ray.tune.schedulers import ASHAScheduler
-from ray.tune.integration.keras import TuneReportCallback
 
 logger = logging.getLogger(__name__)
 
-def _import_gputil():
-    try:
-        import GPUtil
-    except ImportError:
-        GPUtil = None
-    return GPUtil
 
-#Parse arguments usinf argparse
-parser = argparse.ArgumentParser(description='Disk detection complex CNN network',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--train-dataset', type=str,
-                    help='provide the data file path for trianing datasets')
-parser.add_argument('--test-dataset', type=str,
-                    help='provide the data file path for validation datasets')
+class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, alpha=10, warmup_steps=50):
+        super(LRScheduler, self).__init__()
 
-parser.add_argument('--enable-shard', action='store_true', default=False,
-                    help='enable sharding during training')
-parser.add_argument('--enable-amp', action='store_true', default=True,
-                    help='enable automated mixed precision during training')
-parser.add_argument('--enable-xla', action='store_true', default=True,
-                    help='enable tensorflow XLA during training')
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.alpha = alpha
+        self.warmup_steps = warmup_steps
 
-parser.add_argument('--use-checkpoint', action='store_true', default=True,
-                    help='whether to use checkpoint tp load previously trained models ')
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
 
-parser.add_argument('--data-mode', type=str, default='default',
-                    help='set data mode for the training to default, norm or multitask mode')
+        return tf.math.rsqrt(self.alpha * self.d_model) * tf.math.minimum(arg1, arg2)
 
-parser.add_argument('--num-training-iterations', type=int, default=10,
-                    help='set data mode for the training to default, norm or multitask mode')
 
-args = parser.parse_args()
+class TuneLoggerCallback(LoggerCallback):
+    def __init__(self, filename: str = "trial_log.txt"):
+        logger.info("Init LoggerCallback")
+
+    def log_trial_start(self, trial: "Trial"):
+        logger.info("Trial started: {}".format(trial))
+
+    def log_trial_result(self, iiteration: int, trial: "Trial", result: Dict):
+        logger.info("Trial Result for trial: {}, iteration: {} -- result: {}".format(trial, iteration, result))
+
+    def log_trial_end(self, trial: "Trial", failed: bool = False):
+        if failed:
+            logger.warn("Trial fail - trial: {}".format(trial))
+        else:
+            logger.info("Trial success - trial: {}".format(trial))
 
 # %%
 #Build siamese type template matching model for Bragg peak detection
@@ -126,8 +128,8 @@ def build_generator(input_channel, filter_size, n_depth, dp_rate = 0.1, act = 'r
 
     return model
 
+# may not be needed now
 def wait_for_gpu(gpu_id=None, target_util=0.001, retry = 10, delay_s=10):
-    GPUtil = _import_gputil()
     if GPUtil is None:
         raise RuntimeError(
             "GPUtil must be installed if calling `wait_for_gpu`.")
@@ -186,52 +188,46 @@ def wait_for_gpu(gpu_id=None, target_util=0.001, retry = 10, delay_s=10):
 
 # %%
 #template matching using Siamese identical u-net
-def train_model(config):
+def train_model(config, checkpoint_dir=None):
     '''
     train loop
     config: filter_size, depth, dropout, activation, alpha, batch_size, warmup, optimizer,
     '''
-    
-    print('Checking if GPU is available before trial starts... \n')    
-    #TODO: Build a new wait for gpu
-    wait_for_gpu()
-    
-    #Load Tensorflow on GPUs
+
     print('Loading Tensorflow GPU: \n')
+    # issue with ray actors with connecting to the autograph
     import tensorflow as tf
-    import tensorflow_addons as tfa
 
-    print('Loading complex convolution, pooling libraries ... \n')
+    print("train_model")
 
-    #Custom libraries - User defined
-    import convSpectral
-    import poolSpectral
-    import augmentation
-    from conv_utils import parseDataset
-    from conv_utils import convSpec2d_block, conv2d_block
-    from conv_utils import cross_correlate_iff, cross_correlate_ff
-    from conv_utils import CustomSSIML1ELoss
-    
+    #print('Checking if GPU is available before trial starts... \n')
+    #TODO: Build a new wait for gpu
+    #wait_for_gpu()
+
+    # necessary to prevent workers from exhausting threads on the host
+    tf.config.threading.set_intra_op_parallelism_threads(1)
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+
     #Enable Automatic Mixed Precision
-    if args.enable_amp:
-        print("Enabling mixed precision... \n")
-        from tensorflow.keras import mixed_precision
-        policy = mixed_precision.Policy('mixed_float16')
-        mixed_precision.set_global_policy(policy)
+    #if args.enable_amp:
+    #    print("Enabling mixed precision... \n")
+    #    from tensorflow.keras import mixed_precision
+    #    policy = mixed_precision.Policy('mixed_float16')
+    #    mixed_precision.set_global_policy(policy)
 
     #Load Tensorflow XLA
-    if args.enable_xla:
-        print("Enabling Tensorflow XLA... \n")
-        tf.keras.backend.clear_session()
-        tf.config.optimizer.set_jit(True) # Enable XLA.
+    #if args.enable_xla:
+    #    print("Enabling Tensorflow XLA... \n")
+    #    tf.keras.backend.clear_session()
+    #    tf.config.optimizer.set_jit(False) # Enable XLA.
     
     #Load TFRecords to TF batch dataset
     #TODO: Use TF Generators
-    batch_size = 8
-    prepTrainDataset = parseDataset(filepath = args.train_dataset)
-    train_dataset = prepTrainDataset.read(batch_size = batch_size, augment = False, mode = args.data_mode)
-    prepTestDataset = parseDataset(filepath = args.test_dataset)
-    val_dataset = prepTestDataset.read(batch_size = batch_size, augment = False, mode = args.data_mode)
+    batch_size = 4
+    prepTrainDataset = parseDataset(filepath = config["train_dataset"])
+    train_dataset = prepTrainDataset.read(batch_size = batch_size, augment = False, mode = config["data_mode"])
+    prepTestDataset = parseDataset(filepath = config["test_dataset"])
+    val_dataset = prepTestDataset.read(batch_size = batch_size, augment = False, mode = config["data_mode"])
 
     # Disable AutoShard.
     if not args.enable_shard:
@@ -239,25 +235,6 @@ def train_model(config):
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
         train_dataset = train_dataset.with_options(options)
         val_dataset = val_dataset.with_options(options)
-    
-    input_channel = 2
-    EPOCHS =10
-    model = build_generator(input_channel, config["filter_size"], config["depth"], dp_rate = config["dropout"], act = config["activation"])
-    
-    class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
-        def __init__(self, d_model, alpha =10, warmup_steps=50):
-            super(LRScheduler, self).__init__()
-
-            self.d_model = d_model
-            self.d_model = tf.cast(self.d_model, tf.float32)
-            self.alpha = alpha
-            self.warmup_steps = warmup_steps
-
-        def __call__(self, step):
-            arg1 = tf.math.rsqrt(step)
-            arg2 = step * (self.warmup_steps ** -1.5)
-
-            return tf.math.rsqrt(self.alpha*self.d_model) * tf.math.minimum(arg1, arg2)
     
     learning_rate = LRScheduler( batch_size, alpha =  config["alpha"], warmup_steps = config["warmup"])
     
@@ -269,51 +246,145 @@ def train_model(config):
         optimizer = tf.keras.optimizers.Adadelta(learning_rate= learning_rate)
     elif config["optimizer"] == 'lamb':
         optimizer = tfa.optimizers.LAMB(learning_rate= learning_rate)
-     
-    model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
-    model.fit(train_dataset, callbacks=[TuneReportCallback({
-            "mean_mae": "mae"})], epochs=EPOCHS, validation_data = val_dataset)
-    
-def tune_model(num_training_iterations):
+    else:
+        # TODO - set default here or before the if clause
+        logger.warning("optimizer not set!")
+        logger.info("Setting optimizer to default of 'adam'")
+        optimizer = tf.keras.optimizers.Adam(learning_rate=  learning_rate)
+
+    with strategy.scope():
+        input_channel = 2
+        model = build_generator(input_channel, config["filter_size"], config["depth"], dp_rate = config["dropout"], act = config["activation"])
+        model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+
+    #tboard_callback = tf.keras.callbacks.TensorBoard(log_dir='/global/cscratch1/sd/mhenders/ncem_ml_runs/', histogram_freq=1,profile_batch = '1,1000')
+
+    print("Calling model.fit")
+    EPOCHS = 10
+    model.fit(
+        train_dataset,
+        callbacks=[TuneReportCheckpointCallback({"mean_mae": "mae"})],
+        epochs=EPOCHS,
+        validation_data = val_dataset)
+
+def tune_model(args):
+    print("tune model")
+    print(args)
+
+    scratch_dir = os.path.abspath(os.path.expandvars("$SCRATCH"))
+    run_dir = os.path.join(scratch_dir, "ncem_ml_runs")
+
+    num_workers = args.num_ray_hosts * args.num_ray_workers_per_host
     sched = ASHAScheduler(time_attr="training_iteration", max_t=400, grace_period=20)
     hyperopt_search = HyperOptSearch(metric="mean_mae", mode="min")
-   # sched = ASHAScheduler(metric="mean_accuracy", mode="max")
+    hyperopt_search = ConcurrencyLimiter(hyperopt_search, max_concurrent=num_workers)
+    # sched = ASHAScheduler(metric="mean_accuracy", mode="max")
 
-    analysis = tune.run(
+    tf.keras.backend.clear_session()
+    tf.config.run_functions_eagerly(False)
+    tf.config.optimizer.set_jit(False)
+
+    # with tf.profiler.experimental.Trace(
+    #    ','.join(node_ips), '/global/cscratch1/sd/mhenders/ncem_ml_runs/', 60):
+    tf_trainable = DistributedTrainableCreator(
         train_model,
-        name="exp",
+        num_workers=num_workers,
+        num_workers_per_host=args.num_ray_workers_per_host,
+        num_gpus_per_worker=args.num_ray_gpus_per_worker,
+        num_cpus_per_worker=args.num_ray_cpus_per_worker)
+    analysis = tune.run(
+        tf_trainable,
+        verbose=2,
+        name="raytune_experiment",
+        local_dir=run_dir,
+        max_failures=10,
+        queue_trials=False,
+        log_to_file=True,
+        progress_reporter=CLIReporter(),
         scheduler=sched,
         search_alg=hyperopt_search,
         metric="mae",
         mode="min",
         stop={
             "mean_mae": 0.1,
-            "training_iteration": num_training_iterations
-        },
+            "training_iteration": args.num_training_iterations
+            },
         num_samples=-1,
-        checkpoint_at_end=True,
-        resources_per_trial={
-            "cpu": 10,
-            "gpu": 1
-        },
+        # checkpoint_at_end=True,
+        sync_config=tune.SyncConfig(sync_to_driver=False),
         config={
+            "train_dataset": args.train_dataset,
+            "test_dataset": args.test_dataset,
+            "data_mode": args.data_mode,
             "filter_size": tune.randint(4, 64),
             "depth": tune.randint(2, 8),
             "dropout": tune.uniform(0.1, 0.5),
             "momentum": tune.uniform(0.1, 0.9),
-            "alpha": tune.randint(1,10000),
-            "warmup": tune.randint(1,50),
+            "alpha": tune.randint(1, 10000),
+            "warmup": tune.randint(1, 50),
             "activation": tune.choice(["relu", "tanh"]),
             "optimizer": tune.choice(["adam", "sgd", "adadelta", "lamb"])
-        })
-    
-    print("Best hyperparameters found were: ", analysis.best_config)
+            },
+        # resources_per_trial=tune.PlacementGroupFactory([{"CPU": args.num_ray_cpus_per_worker, "GPU": args.num_ray_gpus_per_worker}]),
+        callbacks=[TuneLoggerCallback()])
+    return analysis
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level = logging.DEBUG)
+
+    # Parse arguments usinf argparse
+    parser = argparse.ArgumentParser(description='Disk detection complex CNN network',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train-dataset', type=str,
+                        help='provide the data file path for trianing datasets')
+    parser.add_argument('--test-dataset', type=str,
+                        help='provide the data file path for validation datasets')
+
+    parser.add_argument('--enable-shard', action='store_true', default=False,
+                        help='enable sharding during training')
+    parser.add_argument('--enable-amp', action='store_true', default=True,
+                        help='enable automated mixed precision during training')
+    parser.add_argument('--enable-xla', action='store_true', default=True,
+                        help='enable tensorflow XLA during training')
+
+    parser.add_argument('--use-checkpoint', action='store_true', default=True,
+                        help='whether to use checkpoint tp load previously trained models ')
+
+    parser.add_argument('--data-mode', type=str, default='default',
+                        help='set data mode for the training to default, norm or multitask mode')
+
+    parser.add_argument('--num-training-iterations', type=int, default=1,
+                    help='Number of training iterations')
+
+    parser.add_argument('--num-ray-hosts', type=int, default=8,
+                    help='Number of workers')
+
+    parser.add_argument('--num-ray-workers-per-host', type=int, default=8,
+                    help='Number of workers per host')
+
+    parser.add_argument('--num-ray-cpus-per-worker', type=int, default=9,
+                    help='Number of cpus per worker')
+
+    parser.add_argument('--num-ray-gpus-per-worker', type=int, default=1,
+                    help='Number of gpus per worker')
+
+    args = parser.parse_args()
+
+    scratch_dir = os.path.abspath(os.path.expandvars("$SCRATCH"))
+
     # ip_head and redis_passwords are set by ray cluster shell scripts
     print(os.environ["ip_head"], os.environ["redis_password"])
-    ray.init(address='auto', _node_ip_address=os.environ["ip_head"].split(":")[0], _redis_password=os.environ["redis_password"])
-    import pathlib
-    print(pathlib.Path(__file__).parent.absolute())
-    print(pathlib.Path().absolute())
-    tune_model(args.num_training_iterations)
+    ray.init(
+        address='auto',
+        log_to_driver=True,
+        _redis_max_memory=100*10**9,
+        #_node_ip_address=os.environ["ip_head"].split(":")[0],
+        _redis_password=os.environ["redis_password"],
+        _temp_dir=os.path.join(scratch_dir, 'raytmp')
+    )
+    #import pathlib
+    #print(pathlib.Path(__file__).parent.absolute())
+    #print(pathlib.Path().absolute())
+    analysis = tune_model(args)
+    print("Best hyperparameters found were: ", analysis.best_config)
